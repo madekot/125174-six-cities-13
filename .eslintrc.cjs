9ac7fae75bc4959a233ccbd584d3b4682dd2eb13
Mikhail Golubtsov <mgolubcov@gmail.com>
2023-09-03 19:37:52 +0300

    feat: добавляет prettier и настройки

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     "htmlacademy/react-typescript",
+    "prettier"
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: 'tsconfig.json' },
